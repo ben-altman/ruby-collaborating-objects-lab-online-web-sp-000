98a27bb1e67e8c41447daacb7e0de08fe164466a
Ben Altman <altmanguitar@gmail.com>
2021-01-21 18:19:14 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -15,7 +15,7 @@ class Song
   end
 
   def self.new_by_filename(filename)
-    song = self.new
+    song = self.new(name)
     filename.split(" - ")
     song.artist = filename[0]
     song.name = filename[1]
