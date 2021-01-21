1689cc9b81f1457ed4d7ec98058862c3cb865d60
Ben Altman <altmanguitar@gmail.com>
2021-01-21 17:40:16 +0000

    Automatically backed up by Learn

---
+++ lib/mp3_importer.rb
@@ -0,0 +1,18 @@
+#parse filenames and send filenames to Song class
+
+class MP3Importer
+  attr_accessor :path
+
+  def initialize(path)
+    @path = path
+  end
+
+  def files
+    Dir.entries(".spec/fixtures")
+
+  end
+
+  def import
+    files.each {|filename| Song.new_by_filename(filename)}
+  end
+end
---
+++ lib/song.rb
@@ -0,0 +1,24 @@
+#create songs given each filename
+#send the artist's name (string) to Artist class
+
+class Song
+  attr_accessor :name, :artist
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.new_by_filename(filename)
+    filename.split(" - ")
+  end
+
+  def artist_name=(artist_name)
+    self.artist = Artist.find_or_create_by_name(artist_name)
+  end
+end
